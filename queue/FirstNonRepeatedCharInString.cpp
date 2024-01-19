a2a052a84c6360ad3904041916f99e468dbafe9a
prashuk jain <29jainprashuk@gmail.com>
2024-01-19 14:37:58 +0530

    queues

--- queue/FirstNonRepeatedCharInString.cpp
+++ queue/FirstNonRepeatedCharInString.cpp
@@ -1,2 +1,6 @@
 // time : o(n)
-// space : o(n)
+// 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
+// // space : o(n)
+// const accessToken = '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';
+// const owner = 'Jainprashuk';
+// const repo = 'Data-Structure-Algorithm';
---
+++ queue/GasStation.cpp
@@ -0,0 +1,2 @@
+// approch 1 - two loop - o(N)
+// in approach 1 we are checking some cases without any reson so  ie when we cant go to till from start we can directly put our first loop to third station
